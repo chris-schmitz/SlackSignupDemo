4865648bdeada64c2ddfdb3441da5e7c8671287d
cschmitz <schmitz.chris@gmail.com>
2016-05-11 09:29:02 -0500

    Fixes bug in Signup controller re: returned record count. Adds logic into controller for handling invite resending.

--- SlackSignup/app/Http/Controllers/SlackSignupController.php
+++ SlackSignup/app/Http/Controllers/SlackSignupController.php
@@ -8,25 +8,65 @@ use Illuminate\Http\Response;
 
 class SlackSignupController extends Controller
 {
+    protected $signups;
+    protected $request;
+
+    public function __construct(Signup $signups, Request $request)
+    {
+        $this->signups = $signups;
+        $this->request = $request;
+    }
 
     public function create()
     {
         return view('slack.signup.create');
     }
 
-    public function store(Request $request, Signup $signups)
+    public function store()
     {
-        // validation
-        $data = $request->all();
+        // add to validation
+        // at least one invite
+        $this->validate($this->request, [
+            'email' => 'required|email', // note that we're not using the unique validator. If the email already exists we want to give the option to resend invites.
+        ]);
 
-        $signupData = [
-            'first_name' => $data['name']['first'],
-            'last_name' => $data['name']['last'],
-            'email' => $data['email'],
-        ];
+        $resendInvites = $this->request->get('resend') ?: false;
+        $email = $this->request->get('email');
 
-        $signups->persist($signupData);
+        if ($this->hasAlreadySignedUp($email) && !$resendInvites) {
+            return $this->response("You have already submitted a signup request.", 409);
+        }
 
-        return $this->response('Signup successful.', 500);
+        $message = $this->handleInviteRequest($resendInvites);
+
+        // fire event "SuccessfulSignup" // events send out invites and do whatever accompanying logic we want that is ancillary to this request
+
+        return $this->response($message, 200);
+    }
+
+    protected function hasAlreadySignedUp($email)
+    {
+        return $this->signups->exists($email)->count() > 0;
+    }
+
+    protected function handleInviteRequest($resendInvites = false)
+    {
+        $message = null;
+
+        if (!$resendInvites) {
+
+            $signupData = [
+                'first_name' => $this->request->get('name')['first'],
+                'last_name' => $this->request->get('name')['last'],
+                'email' => $this->request->get('email'),
+            ];
+
+            $this->signups->persist($signupData);
+            $message = 'Signup successful.';
+
+        } else {
+            $message = 'Invites resent.';
+        }
+        return $message;
     }
 }
--- SlackSignup/app/Models/Signup.php
+++ SlackSignup/app/Models/Signup.php
@@ -5,6 +5,18 @@ namespace App\Models;
 class Signup extends UuidModel
 {
     protected $fillable = ['first_name', 'last_name', 'email'];
+    protected $records = [];
+
+    public function exists($email)
+    {
+        $this->records = $this->where('email', $email)->first();
+        return $this;
+    }
+
+    public function count()
+    {
+        return count($this->records);
+    }
 
     public function persist($signupData)
     {
