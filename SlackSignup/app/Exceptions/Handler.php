a8fb96d7180a45f411077cde04bf4ca205bccc6a
cschmitz <schmitz.chris@gmail.com>
2016-06-04 08:21:59 -0500

    Refactors error handling to mask messages from fatal internal and sqs exceptions

--- SlackSignup/app/Exceptions/Handler.php
+++ SlackSignup/app/Exceptions/Handler.php
@@ -3,12 +3,16 @@
 namespace App\Exceptions;
 
 use App\Traits\ReturnsHttpResponse;
+use Aws\Sqs\Exception\SqsException;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\Debug\Exception\FatalErrorException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
@@ -37,7 +41,18 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
-
+        if ($e instanceof SqsException) {
+            // consider refactoring the NotifyAdmins listener to accept a more generic event
+            // and then fire an event from here called `SqsExceptionEncountered`, or maybe
+            // create a job that is a generic "Notify Admins" that both the `NotifyAdmins` and
+            // this report can hand off to.
+            // Regardless of how you accomplish it, you shouldn't pack all of this in here.
+            Mail::raw(sprintf('There was an sqs exception at %s. Message: %s', Carbon::now(), $e->getMessage()), function ($m) {
+                $m->to('schmitz.chris@gmail.com', "Chris Schmitz")
+                    ->from('chris.schmitz.dev@gmail.com', "Slack Signup demo")
+                    ->subject('SQS Exception');
+            });
+        }
         parent::report($e);
     }
 
@@ -50,8 +65,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        $message = $e->getMessage();
+        if ($e instanceof FatalErrorException || $e instanceof SqsException) {
+            $message = config("messages.exceptions.internalError");
+        }
+
         if ($request->wantsJson()) {
-            return $this->response($e->getMessage(), $e->getStatusCode());
+            return $this->response($message, 500);
         }
         return parent::render($request, $e);
     }
---
+++ SlackSignup/config/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'exceptions' => [
+        'internalError' => "There has been an internal error. Please contact the meetup group for help.",
+    ],
+];
